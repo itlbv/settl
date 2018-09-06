fafd47c66e9d98d49910b7d9c3a953930256988e
itlbv <artem.v.fokin@gmail.com>
2018-09-06 15:11:40 +0200

    First commit

---
+++ core/src/com/itlbv/settl/Game.java
@@ -0,0 +1,34 @@
+package com.itlbv.settl;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class Game extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture img;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		img = new Texture("cat.png");
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glClearColor(0, 1, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.begin();
+		batch.draw(img, 0, 0);
+		batch.end();
+
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+		img.dispose();
+	}
+}
---
+++ desktop/src/com/itlbv/settl/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package com.itlbv.settl.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.itlbv.settl.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 1200;
+		config.height = 1200;
+		new LwjglApplication(new Game(), config);
+	}
+}
