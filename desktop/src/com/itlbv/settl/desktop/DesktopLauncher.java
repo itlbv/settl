6f63a4802be1dc774fac5e930fd19fdaca9e11f3
itlbv <artem.v.fokin@gmail.com>
2018-09-08 01:10:13 +0200

    got rid of magical numbers in initial screen config, constants added

--- desktop/src/com/itlbv/settl/desktop/DesktopLauncher.java
+++ desktop/src/com/itlbv/settl/desktop/DesktopLauncher.java
@@ -5,10 +5,13 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.itlbv.settl.Game;
 
 public class DesktopLauncher {
+	private static final int INITIAL_SCREEN_WIDTH_PXL = 1600;
+	private static final int INITIAL_SCREEN_HEIGHT_PXL = 1200;
+
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = 1200;
-		config.height = 1200;
+		config.width = INITIAL_SCREEN_WIDTH_PXL;
+		config.height = INITIAL_SCREEN_HEIGHT_PXL;
 		new LwjglApplication(new Game(), config);
 	}
 }
