18df0c61683727d9d8bf8cf036c8b930a78aa884
itlbv <artem.v.fokin@gmail.com>
2018-10-09 16:45:46 +0200

    Fix mob's collision

--- core/src/com/itlbv/settl/CollisionHandler.java
+++ core/src/com/itlbv/settl/CollisionHandler.java
@@ -7,36 +7,50 @@ import com.badlogic.gdx.physics.box2d.Manifold;
 import com.itlbv.settl.mobs.Mob;
 
 public class CollisionHandler implements ContactListener {
+
+    private Object o1;
+    private Object o2;
+
     @Override
     public void beginContact(Contact contact) {
-        Object o1 = contact.getFixtureA().getBody().getUserData();
-        Object o2 = contact.getFixtureB().getBody().getUserData();
+        o1 = contact.getFixtureA().getBody().getUserData();
+        o2 = contact.getFixtureB().getBody().getUserData();
+        //printCollision(contact);
+        if (o1 == o2) {
+            return;
+        }
+        if (!collisionOfMobs()){
+            return;
+        }
+        if (contact.getFixtureA().isSensor()) {
+            solveCollisionForSensor(o1, o2);
+        } else if (contact.getFixtureB().isSensor()) {
+            solveCollisionForSensor(o2, o1);
+        }
+    }
 
+    private boolean collisionOfMobs() {
+        return (o1 instanceof Mob) || (o2 instanceof Mob);
+    }
+
+    private void solveCollisionForSensor(Object sensorOwnerObj, Object mobOfCollisionObj) {
+        Mob sensorOwner = (Mob) sensorOwnerObj;
+        Mob mobOfCollision = (Mob) mobOfCollisionObj;
+        if (sensorOwner.getTarget() == mobOfCollision) {
+            sensorOwner.setTargetWithinReach(true);
+            System.out.println(sensorOwner.getClass().getSimpleName()
+                    + " reached target "
+                    + mobOfCollision.getClass().getSimpleName());
+        }
+    }
+
+    private void printCollision(Contact contact) {
         System.out.println(o1.getClass().getSimpleName()
                 + (contact.getFixtureA().isSensor() ? "SENSOR" : "BODY")
                 + "  ---contacting--- "
                 + o2.getClass().getSimpleName()
                 + ((contact.getFixtureB().isSensor() ? "SENSOR" : "BODY"))
         );
-
-        if (o1 == o2) {
-            return;
-        }
-        if (!(o1 instanceof Mob) || !(o2 instanceof Mob)){
-            System.out.println("Contact with non-Mob object");
-            return;
-        }
-        Mob m1 = (Mob) o1;
-        Mob m2 = (Mob) o2;
-
-        if (m1.getTarget() == m2) {
-            m1.setTargetWithinReach(true);
-            System.out.println("Target in reach");
-        }
-        if (m2.getTarget() == m1) {
-            m2.setTargetWithinReach(true);
-            System.out.println("Target in reach");
-        }
     }
 
     @Override
