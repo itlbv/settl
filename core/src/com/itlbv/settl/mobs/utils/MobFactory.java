1fdbfcbf7c5f72291a9a02d8b02830428a3fccc4
itlbv <artem.v.fokin@gmail.com>
2018-10-20 22:54:33 +0200

    Refactoring

--- core/src/com/itlbv/settl/mobs/utils/MobFactory.java
+++ core/src/com/itlbv/settl/mobs/utils/MobFactory.java
@@ -2,41 +2,77 @@ package com.itlbv.settl.mobs.utils;
 
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.itlbv.settl.Game;
+import com.itlbv.settl.GameWorld;
 import com.itlbv.settl.enumsObjectType.MobObjectType;
-import com.itlbv.settl.map.Map;
+import com.itlbv.settl.map.Node;
 import com.itlbv.settl.mobs.HumanKnight;
 import com.itlbv.settl.mobs.HumanPeasant;
 import com.itlbv.settl.mobs.Mob;
 import com.itlbv.settl.mobs.OrcShaman;
+import com.itlbv.settl.util.BodyFactory;
 
 public class MobFactory {
-    public static Mob createMob(float x, float y, MobObjectType type) {
-        switch (type) {
-            case HUMAN_PEASANT:
-                return new HumanPeasant(x, y);
-            case HUMAN_KNIGHT:
-                return new HumanKnight(x, y);
-            case ORC_SHAMAN:
-                return new OrcShaman(x, y);
-            default:
-                return new HumanPeasant(x, y);
-        }
+    public static Mob createMobAtRandomPosition(boolean rightMapSide, MobObjectType type) {
+        int mapWidth = Game.map.getWidth();
+        int mapHeight = Game.map.getHeight();
+        int randX, randY;
+        do {
+            randX = MathUtils.random(mapWidth/2 - 1);
+            randY = MathUtils.random(mapHeight - 1);
+            if (rightMapSide) randX += mapWidth/2;
+        } while (mapTileIsNotPassable(randX, randY));
+        return createMob(randX, randY, type);
     }
 
-    public static Mob createMobAtRandomPosition(boolean upperMapSide, MobObjectType type) {
-        float randX = MathUtils.random(25);
-        float randY = MathUtils.random(55);
-        if (upperMapSide) {
-            randX += 25;
-        }
-        while (Map.getInstance().getTileFromPosition(new Vector2(randX, randY)).isCollidable()) {
-            randX = MathUtils.random(25);
-            randY = MathUtils.random(55);
-            if (upperMapSide) {
-                randX += 25;
-            }
-        }
+    private static boolean mapTileIsNotPassable(int x, int y) {
+        return !Game.map.getNodeFromPosition(new Vector2(x, y)).isPassable();
+    }
 
-        return createMob(randX, randY, type);
+    private static Mob createMob(float x, float y, MobObjectType type) {
+        Mob mob;
+        String bhvTree;
+        float bodyWidth, bodyHeight, sensorWidth, sensorHeight;
+        switch (type) {
+            case HUMAN_PEASANT:
+                mob = new HumanPeasant(x, y);
+                bhvTree = "bhvTrees/humanKnight.btree";
+                bodyWidth = MobConstants.HUMAN_BODY_WIDTH;
+                bodyHeight = MobConstants.HUMAN_BODY_HEIGHT;
+                sensorWidth = MobConstants.HUMAN_SENSOR_WIDTH;
+                sensorHeight = MobConstants.HUMAN_SENSOR_HEIGHT;
+                break;
+            case HUMAN_KNIGHT:
+                mob =  new HumanKnight(x, y);
+                bhvTree = "bhvTrees/humanKnight.btree";
+                bodyWidth = MobConstants.HUMAN_BODY_WIDTH;
+                bodyHeight = MobConstants.HUMAN_BODY_HEIGHT;
+                sensorWidth = MobConstants.HUMAN_SENSOR_WIDTH;
+                sensorHeight = MobConstants.HUMAN_SENSOR_HEIGHT;
+                break;
+            case ORC_SHAMAN:
+                mob = new OrcShaman(x, y);
+                bhvTree = "bhvTrees/humanKnight.btree";
+                bodyWidth = MobConstants.ORC_BODY_WIDTH;
+                bodyHeight = MobConstants.ORC_BODY_HEIGHT;
+                sensorWidth = MobConstants.ORC_SENSOR_WIDTH;
+                sensorHeight = MobConstants.ORC_SENSOR_HEIGHT;
+                break;
+            default:
+                mob = new HumanPeasant(x, y);
+                bhvTree = "bhvTrees/humanKnight.btree";
+                bodyWidth = MobConstants.HUMAN_BODY_WIDTH;
+                bodyHeight = MobConstants.HUMAN_BODY_HEIGHT;
+                sensorWidth = MobConstants.HUMAN_SENSOR_WIDTH;
+                sensorHeight = MobConstants.HUMAN_SENSOR_HEIGHT;
+        }
+        Body mobBody = BodyFactory.createBody(BodyDef.BodyType.DynamicBody, bodyWidth, bodyHeight, mob, false);
+        mob.setBody(mobBody, bodyWidth, bodyHeight);
+        Body mobSensor = BodyFactory.createBody(BodyDef.BodyType.DynamicBody, sensorWidth, sensorHeight, mob, true);
+        mob.setSensor(mobSensor);
+        mob.setBhvTree(bhvTree);
+        return mob;
     }
 }
