e6d40abc37461aef4368e77b1c0db1f6e39aa121
itlbv <artem.v.fokin@gmail.com>
2018-10-24 20:14:04 +0200

    Refactoring

--- core/src/com/itlbv/settl/util/CollisionHandler.java
+++ core/src/com/itlbv/settl/util/CollisionHandler.java
@@ -1,6 +1,5 @@
 package com.itlbv.settl.util;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.physics.box2d.Contact;
 import com.badlogic.gdx.physics.box2d.ContactImpulse;
 import com.badlogic.gdx.physics.box2d.ContactListener;
@@ -12,81 +11,35 @@ public class CollisionHandler implements ContactListener {
     private Object o1;
     private Object o2;
 
-    @Override
-    public void beginContact(Contact contact) {
-        o1 = contact.getFixtureA().getBody().getUserData();
-        o2 = contact.getFixtureB().getBody().getUserData();
-        printCollision(contact, true);
-        if (o1 == o2) {
-            return;
-        }
-        if (!collisionOfMobs()){
-            return;
-        }
-        if (contact.getFixtureA().isSensor()) {
-            beginContactForSensor(o1, o2);
-        } else if (contact.getFixtureB().isSensor()) {
-            beginContactForSensor(o2, o1);
-        }
-    }
-
     private boolean collisionOfMobs() {
         return (o1 instanceof Mob) && (o2 instanceof Mob);
     }
 
-    private void beginContactForSensor(Object sensorOwnerObj, Object mobOfCollisionObj) {
-        Mob sensorOwner = (Mob) sensorOwnerObj;
-        Mob mobOfCollision = (Mob) mobOfCollisionObj;
-        if (sensorOwner.getTarget() == mobOfCollision) {
-            sensorOwner.setTargetWithinReach(true);
-            String logStr = sensorOwner.getClass().getSimpleName()
-                    + " reached target "
-                    + mobOfCollision.getClass().getSimpleName();
-            Gdx.app.log(sensorOwner.toString(), logStr);
-        }
-    }
-
-    private void printCollision(Contact contact, boolean begin) {
-        /*
-        String logStr = o1.getClass().getSimpleName()
-                + (contact.getFixtureA().isSensor() ? "SENSOR" : "BODY")
-                + (begin ? "  ---begin--- " : "  ---end--- ")
-                + o2.getClass().getSimpleName()
-                + ((contact.getFixtureB().isSensor() ? "SENSOR" : "BODY"))
-                + " " + Game.RENDER_ITERATION;
-        Gdx.app.log("Contact", logStr);
-
-         */
+    @Override
+    public void beginContact(Contact contact) {
+        o1 = contact.getFixtureA().getBody().getUserData();
+        o2 = contact.getFixtureB().getBody().getUserData();
+        if (o1 == o2) return;
+        if (!collisionOfMobs()) return;
+        if (contact.getFixtureA().isSensor()) processContactForSensor(o1, o2, true);
+        else if (contact.getFixtureB().isSensor()) processContactForSensor(o2, o1, true);
     }
 
     @Override
     public void endContact(Contact contact) {
         o1 = contact.getFixtureA().getBody().getUserData();
         o2 = contact.getFixtureB().getBody().getUserData();
-        printCollision(contact, false);
-        if (o1 == o2) {
-            return;
-        }
-        if (!collisionOfMobs()){
-            return;
-        }
-        if (contact.getFixtureA().isSensor()) {
-            endContactForSensor(o1, o2);
-        } else if (contact.getFixtureB().isSensor()) {
-            endContactForSensor(o2, o1);
-        }
+        if (o1 == o2) return;
+        if (!collisionOfMobs()) return;
+        if (contact.getFixtureA().isSensor()) processContactForSensor(o1, o2, false);
+        else if (contact.getFixtureB().isSensor()) processContactForSensor(o2, o1, false);
     }
 
-    private void endContactForSensor(Object sensorOwnerObj, Object mobOfCollisionObj) {
+    private void processContactForSensor(Object sensorOwnerObj, Object mobOfCollisionObj, boolean beginContact) {
         Mob sensorOwner = (Mob) sensorOwnerObj;
         Mob mobOfCollision = (Mob) mobOfCollisionObj;
-        if (sensorOwner.getTarget() == mobOfCollision) {
-            sensorOwner.setTargetWithinReach(false);
-            String logStr = sensorOwner.getClass().getSimpleName()
-                    + " lost target "
-                    + mobOfCollision.getClass().getSimpleName();
-            Gdx.app.log(sensorOwner.toString(), logStr);
-        }
+        if (sensorOwner.getTarget() != mobOfCollision) return;
+        sensorOwner.setTargetWithinReach(beginContact);
     }
 
     @Override
