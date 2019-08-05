ba31ea6c5a0391e55a6d326fc799f0e65be67f4c
itlbv <artem.v.fokin@gmail.com>
2019-08-05 15:47:25 +0200

    Refactor GameObject with Lombok

--- core/src/com/itlbv/settl/GameObject.java
+++ core/src/com/itlbv/settl/GameObject.java
@@ -6,58 +6,31 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.itlbv.settl.mobs.util.MobConstants;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import static com.itlbv.settl.GameConstants.MOB_TEXTURE_SIZE_PXL;
 
+@Data
+@EqualsAndHashCode(callSuper = false)
 public abstract class GameObject extends SteerableAdapter<Vector2> {
-    //private Vector2 renderPosition;
+
     private Body body;
     private Body sensor;
     private Sprite sprite;
 
-    public GameObject() {
+    protected GameObject() {
         sprite = new Sprite();
     }
 
-    public void updateRenderPosition() {
-        /*
-        float x = body.getPosition().x - renderWidth/2;
-        float y = body.getPosition().y - MobConstants.MOB_BODY_RADIUS;
-        renderPosition.set(x, y);
-        */
+    public void updateSpritePosition() {
         // TODO make check if this is Mob
         float x = body.getPosition().x - sprite.getWidth()/2;
         float y = body.getPosition().y - MobConstants.MOB_BODY_RADIUS;
         sprite.setPosition(x, y);
     }
 
-    //***Getters & setters***
-
-    public Body getBody() {
-        return body;
-    }
-
-    public void setBody(Body body) {
-        this.body = body;
-    }
-
-    public Body getSensor() {
-        return sensor;
-    }
-
-    public void setSensor(Body sensor) {
-        this.sensor = sensor;
-    }
-
-    public Sprite getSprite() {
-        return sprite;
-    }
-
-    public void setSprite(Sprite sprite) {
-        this.sprite = sprite;
-    }
-
-    public Vector2 getPosition() {
+   public Vector2 getPosition() {
         return body.getPosition();
     }
 
@@ -66,6 +39,6 @@ public abstract class GameObject extends SteerableAdapter<Vector2> {
     }
 
     public float getMaxLinearAcceleration() {
-        return 100; //TODO magical number!
+        return 100; //TODO magical number! REMOVE PARAMETER FORM HERE
     }
 }
