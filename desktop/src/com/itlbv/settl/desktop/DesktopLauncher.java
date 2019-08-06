1715c5d696ad55e44f7e4c0f007cb5224e40003e
itlbv <artem.v.fokin@gmail.com>
2019-08-06 14:12:20 +0200

    Migrate to Lwjgl backend version 3

--- desktop/src/com/itlbv/settl/desktop/DesktopLauncher.java
+++ desktop/src/com/itlbv/settl/desktop/DesktopLauncher.java
@@ -1,7 +1,7 @@
 package com.itlbv.settl.desktop;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.itlbv.settl.Game;
 
 public class DesktopLauncher {
@@ -9,9 +9,8 @@ public class DesktopLauncher {
 	private static final int INITIAL_SCREEN_HEIGHT_PXL = 1500;
 
 	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = INITIAL_SCREEN_WIDTH_PXL;
-		config.height = INITIAL_SCREEN_HEIGHT_PXL;
-		new LwjglApplication(new Game(), config);
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setWindowedMode(INITIAL_SCREEN_WIDTH_PXL, INITIAL_SCREEN_HEIGHT_PXL);
+		new Lwjgl3Application(new Game(), config);
 	}
 }
