ba584768b71e3e476b896b8bcd49321a0ecf819b
itlbv <artem.v.fokin@gmail.com>
2019-11-17 17:57:36 +0100

    Change Mob's actions list to Stack, get rid of standalone Target field

--- core/src/com/itlbv/settl/mob/Mob.java
+++ core/src/com/itlbv/settl/mob/Mob.java
@@ -3,30 +3,32 @@ package com.itlbv.settl.mob;
 import com.badlogic.gdx.ai.btree.BehaviorTree;
 import com.badlogic.gdx.ai.btree.utils.BehaviorTreeLibraryManager;
 import com.itlbv.settl.GameObject;
+import com.itlbv.settl.Target;
 import com.itlbv.settl.mob.action.Action;
 import com.itlbv.settl.mob.animation.AnimationProcessor;
 import com.itlbv.settl.mob.movement.Movement;
-import com.itlbv.settl.mob.movement.util.Target;
 import com.itlbv.settl.mob.util.MobType;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.util.LinkedList;
-import java.util.List;
+import java.util.Stack;
 
 @Data
 @EqualsAndHashCode(callSuper = false)
 public class Mob extends GameObject {
 
+    private static final Logger log = LoggerFactory.getLogger(Mob.class);
+
     public Movement movement;
-    public List<Action> actions;
-    public AnimationProcessor animation;
+    private Stack<Action> actions;
+    private AnimationProcessor animation;
     private BehaviorTree<Mob> behavior;
     private int id;
     private MobType type;
     private int hitpoints;
     private boolean alive;
-    private Target target;
     private boolean targetReached;
 
     public Mob(MobType type, String behavior) {
@@ -34,7 +36,7 @@ public class Mob extends GameObject {
         this.hitpoints = type.getHitpoints();
         this.alive = true;
         movement = new Movement(this);
-        actions = new LinkedList<>();
+        actions = new Stack<>();
         animation = new AnimationProcessor(this);
         setBehavior(behavior);
     }
@@ -43,21 +45,39 @@ public class Mob extends GameObject {
         if (hasNoActions()) {
             behavior.step();
         } else {
-            actions.get(0).run();
+            actions.peek().run();
         }
         animation.update();
     }
 
-    private void setBehavior(String behavior) {
-        this.behavior = BehaviorTreeLibraryManager.getInstance().createBehaviorTree(behavior, this);
+    public Action getAction() {
+        if (hasNoActions())
+            return null;
+        return actions.peek();
+    }
+
+    public Action.ActionType getActionType() {
+        if (hasNoActions())
+            return null;
+        return actions.peek().getType();
+    }
+
+    public Target getTarget() {
+        if (hasNoActions())
+            return null;
+        return getAction().getTarget();
     }
 
     public boolean hasNoActions() {
         return actions.isEmpty();
     }
 
+    private void setBehavior(String behavior) {
+        this.behavior = BehaviorTreeLibraryManager.getInstance().createBehaviorTree(behavior, this);
+    }
+
     @Override
     public String toString() {
-        return type.toString() + "_" + id;
+        return "{" + type.toString() + "_" + id + "}";
     }
 }
